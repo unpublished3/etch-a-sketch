a3e2d1a92a0603116d730f5a213370e5e033be85
magn3tism <chapagainsakshyam@gmail.com>
2022-06-28 20:37:18 +0545

    Add core structure

---
+++ main.js
@@ -0,0 +1,9 @@
+const grid = document.getElementById("block-container");
+
+let blocks = [];
+
+for (let i = 0; i < 16 ** 2; i++) {
+  blocks[i] = document.createElement("div");
+  blocks[i].classList.add("block");
+  grid.appendChild(blocks[i]);
+}
