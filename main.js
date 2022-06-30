42f821e11c40b4fb14949d1103463c697a8de735
magn3tism <chapagainsakshyam@gmail.com>
2022-06-30 11:09:36 +0545

    Add slider to adjust size of blocks in gird

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const black = document.getElementById("black");
 const random = document.getElementById("random");
 const custom = document.getElementById("custom");
 
+const slider = document.getElementById("slider");
+
 let blocks = [];
 let colorValue = 1;
 
@@ -20,11 +22,32 @@ function getRandomColor() {
   return color;
 }
 
-// Create and add blocks to grid
-for (let i = 0; i < 16 ** 2; i++) {
+// Update grid size
+function updateGridSize() {
+  grid.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
+}
+
+// Update color of blocks
+function updateColor(block) {
+  if (colorValue === 1) {
+    block.style.backgroundColor = "black";
+  } else if (colorValue === 2) {
+    block.style.backgroundColor = colorPicker.value;
+  } else {
+    block.style.backgroundColor = getRandomColor();
+  }
+}
+
+// Create and add blocks to grid and update color
+for (let i = 0; i < Number(slider.value) ** 2; i++) {
   blocks[i] = document.createElement("div");
   blocks[i].classList.add("block");
   grid.appendChild(blocks[i]);
+
+  blocks[i].addEventListener("mouseover", () => {
+    updateColor(blocks[i]);
+  });
 }
 
 // Change the preferred color
@@ -44,15 +67,20 @@ colorPicker.addEventListener("change", () => {
   colorValue = 2;
 });
 
-// Update color of blocks
-blocks.forEach((block) => {
-  block.addEventListener("mouseover", (e) => {
-    if (colorValue === 1) {
-      e.target.style.backgroundColor = "black";
-    } else if (colorValue === 2) {
-      e.target.style.backgroundColor = colorPicker.value;
-    } else {
-      e.target.style.backgroundColor = getRandomColor();
-    }
-  });
+// Change the size and number of gird on changing slider
+slider.addEventListener("change", (e) => {
+  grid.innerHTML = "";
+
+  blocks = [];
+  updateGridSize();
+
+  for (let i = 0; i < Number(slider.value) ** 2; i++) {
+    blocks[i] = document.createElement("div");
+    blocks[i].classList.add("block");
+    grid.appendChild(blocks[i]);
+
+    blocks[i].addEventListener("mouseover", () => {
+      updateColor(blocks[i]);
+    });
+  }
 });
